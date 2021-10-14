fa806410cd08e9b7ee1d75fd06ee3b05ef3cbc84
tech-codec <varuslimogne@gmail.com>
2021-10-14 23:42:47 +0200

    modification de test

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,6 +1,6 @@
 package ma.enset;
 public class Test{
-    //bonjour le monde
+    //bonjour le monde des dev
     public static void main(String[] args){
         System.out.println("Hello!");
     }
